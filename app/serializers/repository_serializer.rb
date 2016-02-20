a3206ac261c38919f2cef7494bf2c1d3ec129986
Christoph Witzany <christoph@web.crofting.com>
2016-02-20 14:28:32 +0100

    add repository tags to serialization

--- app/serializers/repository_serializer.rb
+++ app/serializers/repository_serializer.rb
@@ -1,5 +1,6 @@
 class RepositorySerializer < ActiveModel::Serializer
   Repository.column_names.each {|pcn| attributes pcn}
 
+  has_many :repository_tags
   has_many :builds, through: :repository_tags
 end
