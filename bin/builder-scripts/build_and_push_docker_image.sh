4d35728ad1b883e1a9952535e42f3a5000b0fb57
Christoph Witzany <christoph@web.crofting.com>
2016-04-03 15:43:14 +0200

    add scripts for builder setup

---
+++ bin/builder-scripts/build_and_push_docker_image.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e
+
+# run as:
+# ./scripts/build_and_push_docker_image.sh \
+#    <git url> \
+#    <git branch> \
+#    <Dockerfile path> \
+#    <tagname>
+
+#TODO: https://github.com/docker-library/node.git /0.10/slim
+
+DIR=$( cd "$( dirname $0 )" && pwd )
+
+echo "Creating working directory ..."
+WORK_DIR=$(mktemp -d)
+echo " - working directory is $WORK_DIR"
+
+REPO_URL=$1
+REPO_BRANCH=$2
+DOCKERFILE_PATH=$3
+IMAGE_NAME=$4
+REGISTRY=$5
+REGISTRY_USER=$6
+REGISTRY_PASSWORD=$7
+REGISTRY_EMAIL=$8
+
+echo "Building image: $IMAGE_NAME" && echo "------------------------------"
+echo " - fetching $REPO_URL ($REPO_BRANCH) to $WORK_DIR"
+git clone --depth=1 --branch $REPO_BRANCH $REPO_URL $WORK_DIR
+cd $WORK_DIR/$DOCKERFILE_PATH
+
+echo " - patching Dockerfile"
+sed -ri "s/FROM\ (.*)/FROM\ $REGISTRY\/\1/g" Dockerfile
+
+echo " - building Docker image as $IMAGE_NAME"
+docker build -t $REGISTRY/$IMAGE_NAME .
+echo " - image built"
+
+
+echo "logging into $REGISTRY"
+docker login -u $REGISTRY_USER -p $REGISTRY_PASSWORD -e $REGISTRY_EMAIL https://$REGISTRY:443
+echo "pushing image"
+docker push $REGISTRY/$IMAGE_NAME
+
+rm -rf $WORK_DIR
+
+(cd $DIR && ./clean_docker_none_images.sh)
