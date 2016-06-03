a9f2fa66e2c34aec221da4a647dbbe21877bf639
Christoph Witzany <christoph@web.crofting.com>
2016-06-03 15:29:41 +0200

    fix secret key

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -1,7 +1,5 @@
 DeviseTokenAuth.setup do |config|
 
-  config.secret_key = ENV['DEVISE_SECRET_KEY']
-
   # By default the authorization headers will change after each request. The
   # client is responsible for keeping track of the changing tokens. Change
   # this to false to prevent the Authorization header from changing after
