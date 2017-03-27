ac89f7771cc4305e04946892dd14875eef33c3cd
Christoph Witzany <christoph@web.crofting.com>
2017-03-27 09:18:28 +0200

    fix call of auth method

--- app/controllers/api/v1/registry_controller.rb
+++ app/controllers/api/v1/registry_controller.rb
@@ -9,14 +9,14 @@ class Api::V1::RegistryController < ApiController
       render text: 'ok'
     else
 
-      authenticate_or_request_with_http_basic(realm: 'marina.io'){ |username, password|
+      authenticate_or_request_with_http_basic(realm: 'marina.io', { |username, password|
         user = User.find_by(name: username)
         if user.valid_password?(password) && user.available_namespaces.include?(params[:namespace])
           render text: 'ok'
         else
           render text: 'forbidden', status: :forbidden
         end
-      }
+      })
     end
   end
 
