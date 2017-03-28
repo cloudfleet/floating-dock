edefea6eb94cf14a2dc02c85b1287186eb10c786
Christoph Witzany <christoph@web.crofting.com>
2017-03-28 21:53:57 +0200

    add debug code

--- app/services/auth/container_registry_authentication_service.rb
+++ app/services/auth/container_registry_authentication_service.rb
@@ -21,6 +21,7 @@ module Auth
     end
 
     def self.full_access_token(*names)
+      puts Rails.configuration.x.marina.docker_registry_jwt_key
       token = JSONWebToken::RSAToken.new(Rails.configuration.x.marina.docker_registry_jwt_key)
       token.issuer = ISSUER
       token.audience = AUDIENCE
