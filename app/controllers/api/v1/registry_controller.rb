e2521db032d735730d4e145859c02b9d016adccd
Christoph Witzany <christoph@web.crofting.com>
2017-03-28 11:00:12 +0200

    add debugging for requests

--- app/controllers/api/v1/registry_controller.rb
+++ app/controllers/api/v1/registry_controller.rb
@@ -2,6 +2,8 @@ class Api::V1::RegistryController < ApiController
   include ActionController::HttpAuthentication::Basic::ControllerMethods
   include ActionController::HttpAuthentication::Basic
 
+  before_action :dump_request
+
   def auth
 
     @repository = Repository.find_by(owner_name: params[:namespace], name: params[:name])
@@ -38,5 +40,20 @@ class Api::V1::RegistryController < ApiController
     else
       render text: 'forbidden', status: :forbidden
     end
+  end
+
+  def dump_request
+    logger.warn "*** FULL PATH ***"
+    logger.warn self.request.fullpath
+    logger.warn "*** RAW HEADERS ***"
+    self.request.env.each do |header|
+      logger.warn "HEADER KEY: #{header[0]}"
+      logger.warn "HEADER VAL: #{header[1]}"
+    end
+    logger.warn "*** RAW BODY ***"
+    logger.warn self.request.raw_post
+    logger.warn "*** END REQUEST ***"
+
+
   end
 end
