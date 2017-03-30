5d8bd2914bbe09bd65a7411ead32836d56261db0
Christoph Witzany <christoph@web.crofting.com>
2017-03-30 12:21:58 +0200

    skip csrf for devise actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
+  skip_before_action :verify_authenticity_token, if: :devise_controller?
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def current_ability
