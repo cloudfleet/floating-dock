63c4917f97c9dafc955c4662aedc4c1a80bd2d83
Christoph Witzany <christoph@web.crofting.com>
2017-08-18 11:39:16 +0200

    adapt rendering of builds for dashboard

--- app/controllers/api/v1/dashboard_controller.rb
+++ app/controllers/api/v1/dashboard_controller.rb
@@ -4,30 +4,46 @@ class Api::V1::DashboardController < ApiController
 
     render json: {
       last_own_builds: last_own_builds,
-      last_global_successful_builds: Build.where(state: 'pushed').last(10)
+      last_global_successful_builds: Build.includes(repository_tag: :repository).where(state: 'pushed').last(10).map{
+        |build|
+        {
+          repository: {
+            name: build.repository.name,
+            owner_name: build.repository.owner_name
+          },
+          tags: [build.repository_tag.name] + build.repository_tag.additional_tags.split(','),
+          architecture: build.architecture,
+          duration: build.end - build.start,
+          pushed: build.end
+        }
+      }
     }
   end
 
   def last_own_builds
     if current_api_v1_user
       namespaces = [current_api_v1_user.name] + current_api_v1_user.organizations.collect(&:name)
-      Build.joins(repository_tag: :repository).where(repositories: {owner_name: namespaces}).last(10)
+      Build.joins(repository_tag: :repository)
+        .includes(repository_tag: :repository)
+        .where(repositories: {owner_name: namespaces})
+        .last(10)
+        .map {
+          |build|
+          {
+            id: build.id,
+            repository: {
+              name: build.repository.name,
+              owner_name: build.repository.owner_name
+            },
+            tags: [build.repository_tag.name] + build.repository_tag.additional_tags.split(','),
+            architecture: build.architecture,
+            state: build.state,
+            duration: build.updated_at - build.start,
+            last_updated: build.updated_at
+          }
+        }
     end
   end
 
-  def stats(namespace)
-    repositories = Repository.where(owner_name: namespace)
-
-    {
-      build_count: {
-        total: repositories.map{|r| r.builds.count}.reduce(:+).to_i,
-        success: repositories.map{|r| r.builds.where(state: 'pushed').count}.reduce(:+).to_i,
-        pending: repositories.map{|r| r.builds.where(end: nil).count}.reduce(:+).to_i,
-        failure: repositories.map{|r| r.builds.where(state: 'failed').count}.reduce(:+).to_i
-      },
-      tags_with_failure: repositories.collect(&:repository_tags).flatten.select{|rt| rt.last_build && rt.last_build.state == "failed"},
-      pending_builds: repositories.collect(&:builds).flatten.select{|b| b.end == nil}
-    }
-  end
 
 end
